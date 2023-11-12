7d9fcb97c36fd8952ecaf535d57cd3888d290829
erickd3v <moyae76@gmail.com>
2023-11-12 01:22:24 -0500

    beta

--- app.js
+++ app.js
@@ -3,12 +3,9 @@ import { routeBeta } from "./backend/routes/beta.js";
 
 
 const app = express();
-const PORT = process.env.PORT ?? 1245
+const PORT = process.env.PORT || 1245;
 
 app.use(express.static('frontend'));
-// app.use('/frontend',static(__dirname + '/frontend'))
-
-// app.use(userRouters)
 
 app.get('/', (req, res) => {
     res.send(`
@@ -28,23 +25,6 @@ app.get('/', (req, res) => {
 });
 
 app.use(routeBeta)
-
-// app.get('/beta', (req, res) => {
-//     res.send(`
-//     <!DOCTYPE html>
-// <html lang="en">
-// <head>
-//     <meta charset="UTF-8">
-//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-//     <title>Document</title>
-//     <link rel="stylesheet" href="/css/estilo.css">
-// </head>
-// <body>
-//     <script type="module" src="./main.js"></script>
-// </body>
-// </html>
-//     `);
-// });
 
 app.listen(PORT, ()=> {
     console.log(`Servidor Express corriendo en el puerto http://localhost:${PORT}`)
